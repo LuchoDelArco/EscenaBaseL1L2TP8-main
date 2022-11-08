7379ebd4130fccc4dbbea498c7137dd76abcc7d7
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-11-08 11:28:37 -0300

    Scene Update

--- Assets/Scripts/NPC/NpcScriptableObject.cs
+++ Assets/Scripts/NPC/NpcScriptableObject.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NpcData", menuName = "NpcData")]
 public class NpcScriptableObject : ScriptableObject
 {
+    [Header("Dialogues")]
     public string[] Dialogues;
     public int[] DialogueInterpelations;
+
+    [Header("Mission State")]
+    public bool isOnMission;
+    public bool isFollowingPlayer;
+    public bool HasFinishedTalking;
+
 
 }
