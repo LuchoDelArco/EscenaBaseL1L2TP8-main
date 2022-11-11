fc23dde4ba3c946380d9f0bb7fde053de9d69282
FacuSalco <facusalco26@gmail.com>
2022-11-11 14:21:42 -0300

    Drone Puzzle Done

---
+++ Assets/Scripts/DRONE/CubeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public GameObject[] Platform = new GameObject[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Platform[0].GetComponent<PlatformScript>().cubeOn && Platform[1].GetComponent<PlatformScript>().cubeOn)
+        {
+            //LUCHO, PONE ACA LO QUE QUERES QUE PASE UNA VEZ QUE EL DRONE YA HIZO LO DE LOS CUBITOS
+            Debug.Log("Ambos presionados");
+        }
+    }
+}
---
+++ Assets/Scripts/DRONE/PlatformScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    public GameObject cube;
+    public bool cubeOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject == cube.gameObject)
+        {
+            col.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            cubeOn = true;
+            col.gameObject.GetComponent<PickableObject>().isPickable = false;
+            col.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            col.gameObject.transform.position = new Vector3(transform.position.x, 0.9442f, transform.position.z);
+            col.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+    //void OnCollisionExit(Collision col)
+    //{
+    //    if (col.gameObject == cube.gameObject)
+    //    {
+    //        col.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    //        cubeOn = false;
+    //    }
+    //}
+
+}
